3820667b8d2496f2284103921a86303450602228
Charlene Auger <charlene.auger@factorfx.com>
2018-12-13 09:36:20 +0100

    Update plugin for extension engine

--- install.php
+++ install.php
@@ -1,32 +1,34 @@
 <?php
-function plugin_version_checkFiles()
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_checkFiles()
 {
-return array('name' => 'Check files',
-'version' => '1.0',
-'author'=> 'Valentin DEVILLE',
-'license' => 'GPLv2',
-'verMinOcs' => '2.2');
+    $commonObject = new ExtensionCommon;
+
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `checkfiles` (
+                          `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                          `HARDWARE_ID` INT(11) NOT NULL,
+                          `PATH` TEXT DEFAULT NULL,
+                          `EXIST` VARCHAR(255) DEFAULT NULL,
+                          PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+                          ) ENGINE=INNODB;");
 }
 
-function plugin_init_checkFiles()
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_checkFiles()
 {
-$object = new plugins;
-$object -> add_cd_entry("checkFiles","other");
-
-$object -> sql_query("CREATE TABLE IF NOT EXISTS `checkfiles` (
-                      `ID` INT(11) NOT NULL AUTO_INCREMENT,
-                      `HARDWARE_ID` INT(11) NOT NULL,
-                      `PATH` TEXT DEFAULT NULL,
-                      `EXIST` VARCHAR(255) DEFAULT NULL,
-                      PRIMARY KEY  (`ID`,`HARDWARE_ID`)
-                      ) ENGINE=INNODB;");
-
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE IF EXISTS `checkfiles`");
 }
 
-function plugin_delete_checkFiles()
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_checkFiles()
 {
-$object = new plugins;
-$object -> del_cd_entry("checkFiles");
-$object -> sql_query("DROP TABLE `checkfiles`");
 
 }
